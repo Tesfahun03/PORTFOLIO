36907d7ae7e5ba33955281665269b6d5cf483a49
Tesfahun03 <tesfa1035@gmail.com>
2022-04-16 17:47:48 +0300

    final comit

--- app.js
+++
@@ -1,8 +0,0 @@
-const menubutton = document.getElementById('menu')
-const navlist = document.getElementById('ul')
-
-function toggleButton(){
-    navlist.classList.toggle('show')
-}
-
-menubutton.addEventListener('click', toggleButton)
